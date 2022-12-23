2fce45f12c655eb6064638277f2a7e42e21c7467
manojkumar <amanojkumar517@gmail.com>
2022-12-24 01:46:42 +0530

    ADDED:code to print the daily wage

--- src/Employee_Wage.java
+++ src/Employee_Wage.java
@@ -1,10 +1,16 @@
 public class Employee_Wage {
     public static void main(String[] args) {
-        int IS_FULL_TIME = 1;
-        double employeeCheck = Math.floor(Math.random() * 10) % 2;
-        if (employeeCheck == IS_FULL_TIME)
+        System.out.println("welcome to Employee wage computation program");
+        final int Wage_per_hour = 20;
+        final int fullday_hours = 8;
+        int pay = 0;
+        double employee = Math.floor(Math.random() * 10) % 2;
+        if (employee == 1) {
             System.out.println(" Employee is present ");
-        else
+            pay = Wage_per_hour * fullday_hours;
+            System.out.println("Employee wage per day is : " + pay);
+        } else {
             System.out.println(" Employee is absent ");
         }
     }
+}
