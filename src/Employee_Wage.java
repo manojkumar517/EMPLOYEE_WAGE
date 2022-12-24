fbe0c43845fc360d42003cb23510d7e88c32901d
manojkumar <amanojkumar517@gmail.com>
2022-12-24 08:41:12 +0530

    ADDED: Code Daily wage using switch case

--- src/Employee_Wage.java
+++ src/Employee_Wage.java
@@ -1,20 +1,22 @@
 public class Employee_Wage {
     public static void main(String[] args) {
-        System.out.println("welcome to Employee wage computation program");
         final int Wage_per_hours = 20;
         final int part_time_hours = 4;
         final int fullday_hours = 8;
         int pay = 0;
-        double employee = Math.floor(Math.random() * 10) % 3;
-        if (employee == 1) {
-            System.out.println(" Employee is full time");
-            pay = Wage_per_hours * fullday_hours;
-        } else if (employee == 2) {
-            System.out.println(" Employee is part time");
-            pay = Wage_per_hours * part_time_hours;
-        } else {
-            System.out.println(" Employee is absent ");
+        int employee = (int) Math.floor(Math.random() * 10) % 3;
+        switch (employee) {
+            case 1:
+                System.out.println(" Employee is full time");
+                pay = Wage_per_hours * fullday_hours;
+                break;
+            case 2:
+                System.out.println(" Employee is part time");
+                pay = Wage_per_hours * part_time_hours ;
+                break;
+            default :
+                System.out.println("Employee is absent");
         }
-        System.out.println("Employee daily wage is:" + pay);
+        System.out.println("Employee daily wage is:"+pay);
     }
 }
